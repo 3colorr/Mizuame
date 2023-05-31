fef4b93e83ae8e0522c9fc465a6299dca8fefc8c
3colorr <3colorr@gmail.com>
2023-05-31 22:54:04 +0900

    Implemented a function for color settings.

--- Mizuame/Settings/ColorSliderView.swift
+++ Mizuame/Settings/ColorSliderView.swift
@@ -8,13 +8,41 @@
 import SwiftUI
 
 struct ColorSliderView: View {
+    let title: String
+    
+    @Binding var rr: Double
+    @Binding var gg: Double
+    @Binding var bb: Double
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 5) {
+            Text(title)
+            
+            VStack {
+                Slider(value: $rr, in: 0.0...1.0) {
+                    Rectangle()
+                        .fill(Color.red)
+                        .frame(width: 10, height: 10)
+                }
+                Slider(value: $gg, in: 0.0...1.0) {
+                    Rectangle()
+                        .fill(Color.green)
+                        .frame(width: 10, height: 10)
+                }
+                Slider(value: $bb, in: 0.0...1.0) {
+                    Rectangle()
+                        .fill(Color.blue)
+                        .frame(width: 10, height: 10)
+                }
+            }
+            .frame(width: 300)
+            .padding(EdgeInsets(top: 5, leading: 10, bottom: 5, trailing: 10))
+        }
     }
 }
 
 struct ColorSliderView_Previews: PreviewProvider {
     static var previews: some View {
-        ColorSliderView()
+        ColorSliderView(title: "ColorSlider", rr: .constant(0.0), gg: .constant(0.0), bb: .constant(0.0))
     }
 }
