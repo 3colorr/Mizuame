9d0e0bbb2ebb084a59044067c681c2944b5fa659
3colorr <3colorr@gmail.com>
2023-10-08 22:18:30 +0900

    Added calculate model.

--- Mizuame/DataModel/CalculateModel.swift
+++ Mizuame/DataModel/CalculateModel.swift
@@ -2,7 +2,44 @@
 //  CalculateModel.swift
 //  Mizuame
 //
-//  Created by becomefoolish on 2023/10/14.
+//  Created by Nakamura Akira(3colorr) on 2023/10/08.
 //
 
 import Foundation
+
+class CalculateModel {
+    
+    // The result function return result of calculate function.
+    // If calculation failed, return nil.
+    public func result(formula: String) -> Double? {
+        
+        let tokens = tokenize(formula: formula)
+        
+        let postfixNotionTokens = postfixNotaion(of: tokens)
+        
+        let result = calculates(it: postfixNotionTokens)
+        
+        return nil
+    }
+    
+    // The tokenize function return tokens.
+    // If failure, return array size zero.
+    private func tokenize(formula: String) -> [String] {
+        
+        return []
+    }
+    
+    // The postfixNotation function return formula in postfix notation.
+    // If failure, return array size zero.
+    private func postfixNotaion(of tokens: [String]) -> [String] {
+        
+        return []
+    }
+    
+    // The calculates function return calculate result.
+    // If calculation failed, return nil.
+    public func calculates(it postNotationTokens: [String]) -> Double? {
+        
+        return nil
+    }
+}
