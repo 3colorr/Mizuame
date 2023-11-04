6e7712f81b2f9fc9bb74dd099a86185f1a928cd1
3colorr <3colorr@gmail.com>
2023-11-04 22:01:25 +0900

    Implemented a calculation function.

--- Mizuame/DataModel/CalculateModel.swift
+++ Mizuame/DataModel/CalculateModel.swift
@@ -9,69 +9,153 @@ import Foundation
 
 class CalculateModel {
     
+    struct token {
+        let value: String
+        let priority: Int
+        let isOperator: Bool
+        let isLeftAssociativity: Bool
+        
+        var operation: (Decimal, Decimal) -> Decimal = {(x: Decimal, y: Decimal) -> Decimal in return 0}
+
+        func number() -> Decimal? {
+            if let n = Decimal(string: value) {
+                return n
+            }
+            
+            return nil
+        }
+    }
+    
     // The result function return result of calculate function.
     // If calculation failed, return nil.
-    public func result(formula: String) -> Double? {
+    public func result(formula: String) -> Decimal? {
         
         let tokens = tokenize(formula: formula)
         
         let postfixNotionTokens = postfixNotaion(of: tokens)
         
-        let result = calculates(it: postfixNotionTokens)
-        
-        return nil
+        return calculates(it: postfixNotionTokens)
     }
     
     // The tokenize function return tokens.
     // If failure, return array size zero.
-    private func tokenize(formula: String) -> [String] {
+    private func tokenize(formula: String) -> [token] {
         
         let splited = Array(formula).map(String.init)
         
-        var token: String = ""
-        var tokens: [String] = []
+        var num: String = ""
+        var tokens: [token] = []
         
         for n in splited {
             switch n {
             case "+":
-                tokens.append(token)
-                tokens.append("+")
-                token = ""
+                if num != "" {
+                    tokens.append(token(value: num, priority: 0, isOperator: false, isLeftAssociativity: false))
+                }
+                tokens.append(token(value: "+", priority: 1, isOperator: true, isLeftAssociativity: true, operation: {(x: Decimal, y: Decimal) -> Decimal in return x + y}))
+                num = ""
             case "-":
-                tokens.append(token)
-                tokens.append("-")
-                token = ""
+                if num != "" {
+                    // (e.g.) 1"-"2
+                    tokens.append(token(value: num, priority: 0, isOperator: false, isLeftAssociativity: false))
+                    tokens.append(token(value: "-", priority: 1, isOperator: true, isLeftAssociativity: true, operation: {(x: Decimal, y: Decimal) -> Decimal in return x - y}))
+                    num = ""
+                } else {
+                    if let m = tokens.last {
+                        if m.value != ")" {
+                            // (e.g.) 1*(-2/3), 1*-2
+                            num = "-"
+                        } else {
+                            // (e.g.) (1+2)-3
+                            tokens.append(token(value: "-", priority: 1, isOperator: true, isLeftAssociativity: true, operation: {(x: Decimal, y: Decimal) -> Decimal in return x - y}))
+                            num = ""
+                        }
+                    } else {
+                        // (e.g.) -1+2
+                        num = "-"
+                    }
+                }
             case "*":
-                tokens.append(token)
-                tokens.append("*")
-                token = ""
+                if num != "" {
+                    tokens.append(token(value: num, priority: 0, isOperator: false, isLeftAssociativity: false))
+                }
+                tokens.append(token(value: "*", priority: 2, isOperator: true, isLeftAssociativity: true, operation: {(x: Decimal, y: Decimal) -> Decimal in return x * y}))
+                num = ""
             case "/":
-                tokens.append(token)
-                tokens.append("/")
-                token = ""
+                if num != "" {
+                    tokens.append(token(value: num, priority: 0, isOperator: false, isLeftAssociativity: false))
+                }
+                tokens.append(token(value: "/", priority: 2, isOperator: true, isLeftAssociativity: true, operation: {(x: Decimal, y: Decimal) -> Decimal in return x / y}))
+                num = ""
             case "^":
-                tokens.append(token)
-                tokens.append("^")
-                token = ""
+                if num != "" {
+                    tokens.append(token(value: num, priority: 0, isOperator: false, isLeftAssociativity: false))
+                }
+                tokens.append(
+                    token(value: "^", priority: 3, isOperator: true, isLeftAssociativity: false,
+                          operation: {
+                              (x: Decimal, y: Decimal) -> Decimal in
+                              let yStr = NSDecimalNumber(decimal: y).stringValue
+                              
+                              if let pointIndex = yStr.firstIndex(of: ".") {
+                                  return Decimal.nan
+                                  // FIX ME!!
+                                  // The following formula cannot calculate "0.01^(-3/2)".
+                                  // If the power is not an integer, return Decimal.nan(not a number) for now.
+                                  /*
+                                   // (e.g.) 2^(1.2)(x=2, y=1.2), 2^(-3.5)(x=2, y=-3.5)
+                                   let yPointDigit = yStr.distance(from: pointIndex, to: yStr.index(before: yStr.endIndex))
+                                   let mm = pow(10, yPointDigit)
+                                   
+                                   if y > 0 {
+                                   // (e.g.) 2^(1.2)(x=2, y=1.2)
+                                   return pow(1 / pow(x, NSDecimalNumber(decimal: mm).intValue), NSDecimalNumber(decimal: y * mm).intValue)
+                                   } else {
+                                   // (e.g.) 2^(-3.5)(x=2, y=-3.5)
+                                   return pow(1 / pow(1 / x, NSDecimalNumber(decimal: mm).intValue), NSDecimalNumber(decimal: -y * mm).intValue)
+                                   }
+                                   */
+                              } else {
+                                  // (e.g.) 2^2(x=2, y=2), 2^(-3)(x=2, y=-3)
+                                  if y > 0 {
+                                      // (e.g.) y=2
+                                      return pow(x, NSDecimalNumber(decimal: y).intValue)
+                                  } else {
+                                      // (e.g.) y=-3
+                                      return 1 / pow(x, NSDecimalNumber(decimal: -y).intValue)
+                                  }
+                              }
+                          }))
+                num = ""
             case "(":
-                tokens.append(token)
-                tokens.append("(")
-                token = ""
+                if num != "" {
+                    tokens.append(token(value: num, priority: 0, isOperator: false, isLeftAssociativity: false))
+                }
+                tokens.append(token(value: "(", priority: 0, isOperator: false, isLeftAssociativity: false))
+                num = ""
             case ")":
-                tokens.append(token)
-                tokens.append(")")
-                token = ""
+                if num != "" {
+                    tokens.append(token(value: num, priority: 0, isOperator: false, isLeftAssociativity: false))
+                }
+                tokens.append(token(value: ")", priority: 0, isOperator: false, isLeftAssociativity: false))
+                num = ""
             case ".":
-                token += n
+                num += n
             default:
                 // Pick up character that can be convert to numeric.
                 // If failure, stop tokenaizing.
                 if Double(n) != nil {
-                    token += n
+                    num += n
                 } else {
                     return []
                 }
             }
+        }
+        
+        // If "num" is not empty, there is a number that have not been append to array.
+        // (e.g.) 1+2+"3" -> the "3" have not been append.
+        if num != "" {
+            tokens.append(token(value: num, priority: 0, isOperator: false, isLeftAssociativity: false))
         }
         
         return tokens
@@ -79,14 +163,92 @@ class CalculateModel {
     
     // The postfixNotation function return formula in postfix notation.
     // If failure, return array size zero.
-    private func postfixNotaion(of tokens: [String]) -> [String] {
+    private func postfixNotaion(of tokens: [token]) -> [token] {
         
-        return []
+        var outputQue: [token] = []
+        var operatorStack: [token] = []
+        
+        for n in tokens {
+            if n.value == "(" {
+                operatorStack.append(n)
+                
+            } else if n.value == ")" {
+                while let op = operatorStack.last {
+                    if op.value == "(" {
+                        operatorStack.removeLast()
+                        break
+                    }
+                    
+                    if let m = operatorStack.popLast() {
+                        outputQue.append(m)
+                    } else {
+                        return []
+                    }
+                }
+                    
+            } else if n.isOperator {
+                while let op = operatorStack.last {
+                    if (op.isOperator && n.isLeftAssociativity && n.priority <= op.priority) || (n.priority < op.priority) {
+                        if let m = operatorStack.popLast() {
+                            outputQue.append(m)
+                        }
+                    } else {
+                        break
+                    }
+                }
+                
+                operatorStack.append(n)
+                
+            } else if Decimal(string: n.value) != Decimal.nan {
+                outputQue.append(n)
+            } else {
+                return []
+            }
+        }
+        
+        while let n = operatorStack.popLast() {
+            if n.value == "(" || n.value == ")" {
+                return []
+            }
+            
+            outputQue.append(n)
+        }
+        
+        return outputQue
     }
     
     // The calculates function return calculate result.
     // If calculation failed, return nil.
-    public func calculates(it postNotationTokens: [String]) -> Double? {
+    public func calculates(it postNotationTokens: [token]) -> Decimal? {
+        
+        var operatorStack: [Decimal] = []
+        
+        for n in postNotationTokens {
+            if n.isOperator {
+                if let x1: Decimal = operatorStack.popLast(), let x2: Decimal = operatorStack.popLast() {
+                    let result: Decimal = n.operation(x2, x1)
+                    
+                    if result == .nan {
+                        return nil
+                    } else {
+                        operatorStack.append(result)
+                    }
+                } else {
+                    return nil
+                }
+                
+            } else {
+                if let m = n.number() {
+                    operatorStack.append(m)
+                } else {
+                    return nil
+                }
+            }
+        }
+        
+        if operatorStack.count == 1 {
+            return operatorStack.last
+        }
         
         return nil
     }
