5b19023a78efc820b3d2b3d629fe5d07ada67d3d
3colorr <3colorr@gmail.com>
2023-12-05 22:41:21 +0900

    Improved a layout of print tab.

--- Mizuame/Settings/TabPrint.swift
+++ Mizuame/Settings/TabPrint.swift
@@ -30,96 +30,138 @@ struct TabPrint: View {
     private let INI_HORIZONTALLY_CENTERED: Bool = SettingKeys.Printer().initialHorizontallyCentered
 
     var body: some View {
-        VStack(alignment: .leading, spacing: 10) {
+        VStack(alignment: .leading, spacing: 20) {
             Text("settings.tab.print.greeting")
 
-            Text("settings.tab.print.margin")
-            ZStack(alignment: .center) {
-                Rectangle()
-                    .fill(Color("PrintMarginImageFill"))
-                    .border(Color("PrintMarginImageBorder"))
-                    .frame(width: 190, height: 180)
-
-                Rectangle()
-                    .stroke(Color("PrintMarginImageBorder"), style: StrokeStyle(lineWidth: 2, dash: [10, 10, 10, 10]))
-                    .frame(width: 100, height: 100)
-
-                VStack {
-                    Text("settings.tab.print.margin.image.top")
+            VStack(alignment: .leading) {
+                Text("settings.tab.print.margin")
+                    .frame(width: 100, alignment: .leading)
+                
+                HStack {
                     Spacer()
-                    Text("settings.tab.print.margin.image.bottom")
+                        .frame(width: 100, alignment: .leading)
+                    
+                    ZStack(alignment: .center) {
+                        Rectangle()
+                            .fill(Color("PrintMarginImageFill"))
+                            .border(Color("PrintMarginImageBorder"))
+                            .frame(width: 60, height: 80)
+                        
+                        Rectangle()
+                            .stroke(Color("PrintMarginImageBorder"), style: StrokeStyle(lineWidth: 2, dash: [5, 5, 5, 5]))
+                            .frame(width: 50, height: 70)
+                        
+                        VStack {
+                            Text("settings.tab.print.margin.image.top")
+                            Spacer()
+                            Text("settings.tab.print.margin.image.bottom")
+                        }
+                        .padding(.vertical, 5)
+                        
+                        HStack {
+                            Text("settings.tab.print.margin.image.left")
+                            Spacer()
+                            Text("settings.tab.print.margin.image.right")
+                        }
+                        .padding(.horizontal, 5)
+                    }
+                    .frame(width: 140, height: 130)
                 }
-                .padding(.vertical, 10)
-
-                HStack {
-                    Text("settings.tab.print.margin.image.left")
-                    Spacer()
-                    Text("settings.tab.print.margin.image.right")
+                
+                VStack(alignment: .leading, spacing: 5) {
+                    HStack {
+                        Spacer()
+                            .frame(width: 100, alignment: .leading)
+                        
+                        Text("settings.tab.print.margin.top")
+                            .frame(width: 50, alignment: .leading)
+                        
+                        TextField("\(INIT_TOP_MARGIN)", value: $imageTopMargin, formatter: NumberFormatter())
+                            .frame(width: 50)
+                            .padding(EdgeInsets(top: 2, leading: 0, bottom: 2, trailing: 0))
+                    }
+                    HStack {
+                        Spacer()
+                            .frame(width: 100, alignment: .leading)
+                        
+                        Text("settings.tab.print.margin.bottom")
+                            .frame(width: 50, alignment: .leading)
+                        
+                        TextField("\(INIT_BOTTOM_MARGIN)", value: $imageBottomMargin, formatter: NumberFormatter())
+                            .frame(width: 50)
+                            .padding(EdgeInsets(top: 2, leading: 0, bottom: 2, trailing: 0))
+                    }
+                    HStack {
+                        Spacer()
+                            .frame(width: 100, alignment: .leading)
+                        
+                        Text("settings.tab.print.margin.left")
+                            .frame(width: 50, alignment: .leading)
+                        
+                        TextField("\(INIT_LEFT_MARGIN)", value: $imageLeftMargin, formatter: NumberFormatter())
+                            .frame(width: 50)
+                            .padding(EdgeInsets(top: 2, leading: 0, bottom: 2, trailing: 0))
+                    }
+                    HStack {
+                        Spacer()
+                            .frame(width: 100, alignment: .leading)
+                        
+                        Text("settings.tab.print.margin.right")
+                            .frame(width: 50, alignment: .leading)
+                        
+                        TextField("\(INIT_RIGHT_MARGIN)", value: $imageRightMargin, formatter: NumberFormatter())
+                            .frame(width: 50)
+                            .padding(EdgeInsets(top: 2, leading: 0, bottom: 2, trailing: 0))
+                    }
                 }
-                .padding(.horizontal, 5)
             }
-            .frame(width: 190, height: 180)
-
-            VStack {
-                HStack {
-                    Text("settings.tab.print.margin.top")
-                    TextField("\(INIT_TOP_MARGIN)", value: $imageTopMargin, formatter: NumberFormatter())
-                        .frame(width: 80)
-                        .padding(EdgeInsets(top: 2, leading: 0, bottom: 2, trailing: 10))
-                }
-                HStack {
-                    Text("settings.tab.print.margin.bottom")
-                    TextField("\(INIT_BOTTOM_MARGIN)", value: $imageBottomMargin, formatter: NumberFormatter())
-                        .frame(width: 80)
-                        .padding(EdgeInsets(top: 2, leading: 0, bottom: 2, trailing: 10))
-                }
-                HStack {
-                    Text("settings.tab.print.margin.left")
-                    TextField("\(INIT_LEFT_MARGIN)", value: $imageLeftMargin, formatter: NumberFormatter())
-                        .frame(width: 80)
-                        .padding(EdgeInsets(top: 2, leading: 0, bottom: 2, trailing: 10))
-                }
-                HStack {
-                    Text("settings.tab.print.margin.right")
-                    TextField("\(INIT_RIGHT_MARGIN)", value: $imageRightMargin, formatter: NumberFormatter())
-                        .frame(width: 80)
-                        .padding(EdgeInsets(top: 2, leading: 0, bottom: 2, trailing: 10))
-                }
-            }
-            .padding(.leading, 0)
 
             HStack {
                 Text("settings.tab.print.scaling")
+                    .frame(width: 100, alignment: .leading)
+
                 TextField("\(INIT_SCALING)", value: $imageScaling, formatter: NumberFormatter())
-                    .frame(width: 80)
-                    .padding(EdgeInsets(top: 2, leading: 0, bottom: 2, trailing: 10))
+                    .frame(width: 50)
+                    .padding(EdgeInsets(top: 2, leading: 0, bottom: 2, trailing: 0))
             }
             
-            Button(action: {
-                imageVerticallyCentered.toggle()
-            }, label: {
-                if imageVerticallyCentered {
-                    Image(systemName: "checkmark.square.fill")
-                } else {
-                    Image(systemName: "square")
+            HStack {
+                VStack {
+                    Text("settings.tab.print.centered.title")
+                    Spacer()
                 }
+                .frame(width: 100, alignment: .leading)
                 
-                Text("settings.tab.print.centered.vertical").font(.body)
-            })
-            .buttonStyle(.plain)
-
-            Button(action: {
-                imageHorizontallyCentered.toggle()
-            }, label: {
-                if imageHorizontallyCentered {
-                    Image(systemName: "checkmark.square.fill")
-                } else {
-                    Image(systemName: "square")
+                VStack(alignment: .leading) {
+                    Button(action: {
+                        imageVerticallyCentered.toggle()
+                    }, label: {
+                        if imageVerticallyCentered {
+                            Image(systemName: "checkmark.square.fill")
+                        } else {
+                            Image(systemName: "square")
+                        }
+                        
+                        Text("settings.tab.print.centered.vertical").font(.body)
+                    })
+                    .buttonStyle(.plain)
+                    
+                    Button(action: {
+                        imageHorizontallyCentered.toggle()
+                    }, label: {
+                        if imageHorizontallyCentered {
+                            Image(systemName: "checkmark.square.fill")
+                        } else {
+                            Image(systemName: "square")
+                        }
+                        
+                        Text("settings.tab.print.centered.horizontal").font(.body)
+                    })
+                    .buttonStyle(.plain)
+                    
+                    Spacer()
                 }
-                
-                Text("settings.tab.print.centered.horizontal").font(.body)
-            })
-            .buttonStyle(.plain)
+            }
             
             Spacer()
             
