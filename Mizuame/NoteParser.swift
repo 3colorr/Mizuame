2d0b097b38859fe03a1d0b46b2e88b0a92d4e005
3colorr <3colorr@gmail.com>
2023-12-22 23:29:30 +0900

    Fixed the parse algorithm.

--- Mizuame/NoteParser.swift
+++ Mizuame/NoteParser.swift
@@ -16,36 +16,65 @@ class NoteParser {
     // If the fomula is not found, return empty array([]).
     public func parse(note: String) -> [Range<String.Index>] {
         var results: [Range<String.Index>] = []
-        var searchRange: Range<String.Index> = note.startIndex..<note.endIndex
-        
-        while let formulaHead: Range<String.Index> = note.range(of: "(", range: searchRange) {
-            
-            let subRange: Range<String.Index> = formulaHead.upperBound..<note.endIndex
-            
-            // Are there '=' and ')'?
-            // If not, end the search.
-            if let rightParenthesisIndex: Range<String.Index> = note.range(of: ")", range: subRange),
-               let equalIndex: Range<String.Index> = note.range(of: "=", range: subRange) {
-                
-                let beforeRightParenthesisIndex: String.Index = note.index(before: rightParenthesisIndex.lowerBound)
-                
-                // If before index of ')' is index of '=', these indexes are considered '=)'.
-                // So the string between 'formulaHead' and 'equalIndex' is the formula.
-                if beforeRightParenthesisIndex == equalIndex.lowerBound {
-                    results.append(formulaHead.upperBound..<equalIndex.lowerBound)
-                }
-                
-                // Decide a next search range.
-                // Compares 'equalIndex' and 'rightParenthesis', use the next search range from the lower index to the end index.
-                if equalIndex.upperBound < rightParenthesisIndex.upperBound {
-                    searchRange = equalIndex.upperBound..<note.endIndex
-                } else {
-                    searchRange = rightParenthesisIndex.upperBound..<note.endIndex
-                }
 
-            } else {
-                // Not found end of formula.
-                break
+        // At first, extract a string that enclosed '(' and '=)' by regular expression.
+        // And then, check if each the extracted string is a formula.
+        note.matches(of: /(\(.+?\=\))/).forEach { matched in
+            
+            let matchedSubstring: Substring = matched.output.1
+
+            // * How to check if the string is a formula? *
+            // Basically, we consider the string between '(' and '=)' as a formula.
+            // But, a results of this method might be incorrect.
+            // If contains '(1+2+3 4 )(5*6=)' in the note, we would expect '(5*6=)' to match,
+            // but '(1+2+3 4 )(5*6=)' matches.
+            // Therefore, we need to remove '(1+2+3 4 )' from '(1+2+3 4 )(5*6=)'.
+            // At first, Increment the index from beginning of matched string while counting number of '('.
+            // If ')' is found, decrement number of '(', and if the number is not 0, continue counting number of '('.
+            // If the number is 0, check if the character before ')' is '='.
+            // If it is '=', the range between '(' and ')' is the formula.
+            // If not, find the new '(' after index of ')', since the range between '(' and ')' is not the formula.
+            // Then, repeat the search.
+            //
+            var headIndex = matchedSubstring.startIndex
+            var searchIndex = matchedSubstring.startIndex
+            var countParenthesis: Int = 0
+            
+            while searchIndex != matchedSubstring.endIndex {
+                let char = matchedSubstring[searchIndex]
+                
+                if char == "(" {
+                    countParenthesis += 1
+                    
+                } else if char == ")" {
+                    countParenthesis -= 1
+                    
+                    if countParenthesis == 0 {
+                        let equalIndex: String.Index = matchedSubstring.index(before: searchIndex)
+                        
+                        if matchedSubstring[equalIndex] == "=" {
+                            // This is the formula.
+                            // Remove the index of '(' and ')'.
+                            let formulaHead = matchedSubstring.index(after: headIndex)
+                            results.append(formulaHead..<equalIndex)
+                            
+                        } else {
+                            // Not the formula
+                            headIndex = matchedSubstring.index(after: searchIndex)
+                            while headIndex != matchedSubstring.endIndex && matchedSubstring[headIndex] != "(" {
+                                headIndex = matchedSubstring.index(after: headIndex)
+                            }
+                            
+                            // Increment the counter because '(' is found.
+                            countParenthesis = 1
+                            
+                            // New search range
+                            searchIndex = headIndex
+                        }
+                    }
+                }
+                
+                searchIndex = matchedSubstring.index(after: searchIndex)
             }
         }
         
