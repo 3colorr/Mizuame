e3f7a2664deeeed2bd482b151fa22533f60fed27
3colorr <3colorr@gmail.com>
2024-04-28 21:37:10 +0900

    The footer is no longer needed.

--- Mizuame/ContentView.swift
+++ Mizuame/ContentView.swift
@@ -421,59 +421,7 @@ struct ContentView: View {
             }
         }
     }
-    
-    private func FooterView() -> some View {
-        HStack {
-            Spacer()
-                .layoutPriority(1)
 
-            Image(systemName: "arrow.up.left.arrow.down.right")
-                .foregroundColor(Color(bodyForegroundTheme))
-                .bold()
-                .onHover { isHover in
-                    if isHover {
-                        NSCursor.closedHand.push()
-                    } else {
-                        NSCursor.pop()
-                    }
-                }
-                .gesture(
-                    DragGesture(minimumDistance: 1)
-                        .updating($dragState) { gestureValue, gestureState, gestureTransaction in
-                            
-                            var updateWidth: CGFloat = gestureValue.translation.width
-                            var updateHeight: CGFloat = gestureValue.translation.height
-                            
-                            let dragWidth: Int = self.width + Int(updateWidth)
-                            let dragHeight: Int = self.height + Int(updateHeight)
-                            
-                            if dragWidth > SettingKeys.StickyNote().maxWidth.intValue {
-                                updateWidth = CGFloat(SettingKeys.StickyNote().maxWidth.intValue)
-                            }
-                            
-                            if dragWidth < SettingKeys.StickyNote().minWidth.intValue {
-                                updateWidth = CGFloat(SettingKeys.StickyNote().minWidth.intValue)
-                            }
-                            
-                            if dragHeight > SettingKeys.StickyNote().maxHeight.intValue {
-                                updateHeight = CGFloat(SettingKeys.StickyNote().maxHeight.intValue)
-                            }
-                            
-                            if dragHeight < SettingKeys.StickyNote().minHeight.intValue {
-                                updateHeight = CGFloat(SettingKeys.StickyNote().minHeight.intValue)
-                            }
-                            
-                            gestureState = CGSize(width: updateWidth, height: updateHeight)
-                        }
-                        .onEnded { endedState in
-                            self.width += Int(endedState.translation.width)
-                            self.height += Int(endedState.translation.height)
-                        }
-                )
-        }
-        .padding(EdgeInsets(top: 5, leading: 10, bottom: 5, trailing: 5))
-    }
-    
     private func userActionDispatcher() {
         switch userAction {
         case .QUIT:
